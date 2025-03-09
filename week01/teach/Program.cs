733cee17b4f50c8837abae3bc21f825d743ea56c
ggosdinski <gianfranco.gosdinski@icloud.com>
2025-03-08 20:09:14 -0500

    Dynamic Arrays Activity

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -11,8 +11,9 @@ ArraySelector.Run();
 
 
 // Uncomment and run as you get to the solution part
-// Console.WriteLine("\n======================\nDivisors Solution\n======================");
-// DivisorsSolution.Run();
+Console.WriteLine("\n======================\nDivisors Solution\n======================");
+DivisorsSolution.Run();
 //
+
 // Console.WriteLine("\n======================\nArray Selector Solution\n======================");
 // ArraySelectorSolution.Run();
